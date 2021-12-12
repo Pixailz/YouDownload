2ab739ca4f0c9951a08fa58073f9fbe09116d1cb
Pixailz <pixailz1@gmail.com>
2021-12-12 16:52:33 +0100

    :sparkles: main.py WindowsUtils(): support installation for ffmpeg

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ def installAndImportModule(package):
     """
 
     import sys
-    import contextlib
     import importlib
     import subprocess
 
@@ -24,13 +23,18 @@ def installAndImportModule(package):
     finally:
         globals()[package] = importlib.import_module(package)
 
-installAndImportModule("pytube")
-installAndImportModule("pygame")
-
+import contextlib
+import subprocess
 import argparse
+import requests
+import shutil
 import time
 import os
 import re
+
+installAndImportModule("zipfile")
+installAndImportModule("pytube")
+installAndImportModule("pygame")
 
  #
 #
@@ -58,6 +62,193 @@ class PosixUtils():
         return path
 
 class WindowsUtils():
+
+    def __init__(self):
+        self.regex = RegexChecker()
+        self.ffmpegCheckInstallation()
+
+    @staticmethod
+    def convert_byte(number, rounded=False):
+
+        """Byte converter"""
+
+        number = int(number)
+
+        if number > 1_000_000_000_000:
+            multiplier = 8 / (8 * 1024 * 1024 * 1024 * 1024)
+            measure_unit = "TiB"
+
+        elif number > 1_000_000_000:
+            multiplier = 8 / (8 * 1024 * 1024 * 1024)
+            measure_unit = "GiB"
+
+        elif number > 1_000_000:
+            multiplier = 8 / (8 * 1024 * 1024)
+            measure_unit = "MiB"
+
+        elif number > 1_000:
+            multiplier = 8 / (8 * 1024)
+            measure_unit = "KiB"
+
+        else:
+            multiplier = 1
+            measure_unit = f"B"
+
+        if rounded:
+            tmp_number = int(round(number * multiplier))
+            converted = f"{tmp_number}{measure_unit}"
+
+        else:
+            tmp_number = round(number * multiplier, 2)
+            converted = f"{tmp_number}{measure_unit}"
+
+        return converted
+
+    @staticmethod
+    def download(link, file_path, title=""):
+
+        """
+        based on this:
+        https://stackoverflow.com/a/21868231/7261056
+        """
+
+        with open(file_path, "wb") as f:
+
+            # download of file
+            response = requests.get(link, stream=True)
+            # get length of ile in header
+            total_length = response.headers.get("content-length")
+
+            # if total length is not present in header
+            if total_length is None:
+                # write without progress bar :'(
+                f.write(response.content)
+
+            else:
+                # cast in int of str
+                total_length = int(total_length)
+                # convert in human readable
+                converted_length = WindowsUtils.convert_byte(total_length, rounded=True)
+
+                # epoch begin time
+                begin_time = time.time()
+
+                # need to be declared out of the loop
+                downloaded_byte = 0
+                str_speed = ""
+                tmp_seconde = 0
+
+                for data in response.iter_content(chunk_size=4096):
+
+                    # add lenght of data in downloaded _byte
+                    downloaded_byte += len(data)
+                    # write data
+                    f.write(data)
+
+                    # get a list of second(0) and millisecond(1)
+                    elapsed_time_brute = str(time.time() - begin_time).split(".")
+
+                    seconde = int(elapsed_time_brute[0])
+                    # cut for 2 digits
+                    ms = elapsed_time_brute[1][:2]
+
+                    # cross product for a length of 25
+                    done = int(25 * downloaded_byte / total_length)
+                    # cross product for pourcentage
+                    pourcentage_done = int(100 * downloaded_byte / total_length)
+
+                    number_of_dot = (seconde % 3) + 1
+                    dot = "." * number_of_dot
+                    space = " " * (3 - number_of_dot)
+                    str_dot = "Download" + dot + space
+
+                    # done str
+                    egal_str = "=" * done
+                    # head str
+                    egal_str += ">"
+                    # empty str
+                    empy_str = " " * (25 - done)
+                    str_progress = f"[{egal_str}{empy_str}]"
+
+                    # elapsed time
+                    minute = seconde // 60
+                    seconde = seconde % 60
+                    str_elapsed = f"[{str(minute).zfill(2)}:{str(seconde).zfill(2)}:{ms}]"
+
+                    # refresh per second
+                    if seconde > tmp_seconde:
+                        tmp_seconde = seconde
+                        # speed convertion
+                        download_speed = WindowsUtils.convert_byte(downloaded_byte / seconde)
+                        str_speed = f"{download_speed}/s"
+
+                    downloaded_done = WindowsUtils.convert_byte(downloaded_byte, rounded=True)
+                    str_downloaded = f"({downloaded_done}/{converted_length}/{pourcentage_done}%)"
+
+                    print(f"\r{str_dot}{str_progress} {title} {str_elapsed} {str_speed} {str_downloaded}", flush=True, end="")
+
+                print(" done")
+
+    @staticmethod
+    def checkIsInstalled():
+        if os.path.isdir("./ffmpeg"):
+            return True
+
+        else:
+            return False
+
+    def checkIsUpToDate(self):
+        with contextlib.redirect_stdout(os.devnull):
+            output = subprocess.getoutput(self._getPrefix() + " -version")
+
+        current_version = self.regex.ffmpegRetrieveVersion(output)
+
+        if current_version == self.latest_version:
+            return 1
+
+        else:
+            return 0
+
+    @staticmethod
+    def ffmpegGetVersion(link):
+        response = requests.get(link + ".ver")
+        return response.content.decode('ascii')
+
+    def ffmpegInstall(self, update=False):
+        if not os.path.isdir("./tmp"):
+            os.mkdir("./tmp")
+
+        self.download(self.link, "./tmp/ffmpeg_latest_release.zip", "ffmpeg")
+
+        with zipfile.ZipFile("./tmp/ffmpeg_latest_release.zip", "r") as zip_ref:
+            zip_ref.extractall("./tmp")
+
+        ffmpeg_folder = f"./tmp/ffmpeg-{self.latest_version}-essentials_build"
+
+        if update:
+            shutil.rmtree("./ffmpeg")
+
+        with contextlib.redirect_stdout(os.devnull):
+            os.rename(ffmpeg_folder, "./ffmpeg/")
+
+        shutil.rmtree("./tmp")
+
+    def ffmpegCheckInstallation(self):
+        self.link = "https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-essentials.zip"
+        self.latest_version = self.ffmpegGetVersion(self.link)
+
+        if self.checkIsInstalled():
+            if self.checkIsUpToDate():
+                print("[ffmpeg] Up to date")
+
+            else:
+                print("[ffmpeg] Outdated, updating.")
+                self.ffmpegInstall(update=True)
+
+        else:
+            self.ffmpegInstall()
+            print("[ffmpeg] Installed")
+
     @staticmethod
     def _getPrefix():
         return ".\\ffmpeg\\bin\\ffmpeg.exe"
@@ -343,5 +534,6 @@ class YoutubeDownloader():
             time.sleep(.25)
 
 if __name__ == "__main__":
-    downloader = YoutubeDownloader()
+    #downloader = YoutubeDownloader()
+    utils = WindowsUtils()
 
